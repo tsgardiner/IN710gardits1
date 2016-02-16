7ca681c53b7e141407e3f23d466245d67aee0e57
tsgardiner <tsgardiner@windowslive.com>
2016-02-16 23:45:12 +1300

    Progress
    Last commit from home PC

--- firstPractical/firstPractical/firstPractical/Form1.cs
+++ firstPractical/firstPractical/firstPractical/Form1.cs
@@ -19,24 +19,30 @@ namespace firstPractical
         }
 
         public void TwoToOne()
-    {
-    const int columnCount = 4;
-    const int rowCount = 2;
+        {
+        const int columnCount = 4;
+        const int rowCount = 2;
 
-	Char[] oneDimensional = new Char[columnCount * rowCount];
-    Char[,] twoDimensional = new Char[columnCount, rowCount] 
+	    Char[] oneDimensional = new Char[columnCount * rowCount];
+        Char[,] twoDimensional = new Char[columnCount, rowCount] 
           { { 'a', 'b' }, { 'c', 'd' }, { 'e', 'f' }, { 'g', 'h' } };
 
-    // convert to one dimension
-    for (int c = 0; c < columnCount; c++)
-        for (int r = 0; r < rowCount; r++)
-        {
-            //Add code here. DO NOT declare any new variables
+        // convert to one dimension
+        for (int c = 0; c < columnCount; c++)
+            for (int r = 0; r < rowCount; r++)
+            {
+                //Add code here. DO NOT declare any new variables
+                oneDimensional[c + r] = twoDimensional[c, r];
+            }
+
+        //printing one dimensional array
+        for (int i = 0; i < columnCount * rowCount; i++)
+        listBox1.Items.Add(oneDimensional[i]);
         }
 
-    //printing one dimensional array
-    for (int i = 0; i < columnCount * rowCount; i++)
-        listBox1.Items.Add(oneDimensional[i]);
-}
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TwoToOne();
+        }
     }
 }
