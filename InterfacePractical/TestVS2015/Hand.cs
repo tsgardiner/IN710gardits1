3ff5a75581ee0112f2e9c83c4c34748db79b4f87
tsgardiner <tsgardiner@windowslive.com>
2016-02-23 16:25:14 +1300

    Interface Practical
    First commit with no changes

---
+++ InterfacePractical/TestVS2015/Hand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TestVS2015
+{
+    public class Hand
+    {
+        public List<Card> CardsInHand { get; set; }
+        public int TotalHCP
+        {
+            get; set;
+        }
+
+        public Hand()
+        {
+            CardsInHand = new List<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            CardsInHand.Add(card);
+        }
+
+        public int ComputeHCP()
+        {
+            TotalHCP = 0;
+            foreach (Card c in CardsInHand)
+                TotalHCP += c.HCP;
+
+            return 0;
+        }
+    }
+}
