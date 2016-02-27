4ce6366e318639ba6e9f8dee85e4305267c69551
tsgardiner <tsgardiner@windowslive.com>
2016-02-27 17:46:26 +1300

    Form Built
    Form created and named
    All button events added to Form

---
+++ CollectionsExceptionsPractical/CollectionsExceptions/CollectionsExceptions/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollectionsExceptions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddMovie_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDisplayAll_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
