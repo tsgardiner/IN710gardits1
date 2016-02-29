6ed33425208d10a45d1eb2f01aad2b13df047eb5
tsgardiner <tsgardiner@windowslive.com>
2016-02-29 13:37:16 +1300

    Display Class
    If not saved gets left from VS commit

---
+++ CollectionsExceptionsPractical/CollectionsExceptions/CollectionsExceptions/DisplayMovies.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollectionsExceptions
+{
+    class DisplayMovies
+    {
+        ListBox lbDisplay;
+
+        public DisplayMovies(ListBox lbDisplay)
+        {
+            this.lbDisplay = lbDisplay;
+        }
+
+        public void  DisplayAll(Dictionary<int ,Movie> movies)
+        {
+
+        }
+
+        public void DisplaySearch(Dictionary<int, Movie> searchedMovie)
+        {
+
+        }
+
+        public void DisplayAddedMovie(Dictionary<int , Movie> addedMovie)
+        {
+
+        }
+
+    }
+}
