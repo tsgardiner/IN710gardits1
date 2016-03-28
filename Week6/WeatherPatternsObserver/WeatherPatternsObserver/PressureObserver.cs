68d4545a62ad6e93e2f3538867e2d5f2b33516f1
tsgardiner <tsgardiner@windowslive.com>
2016-03-28 23:36:38 +1300

    Observer templates

---
+++ Week6/WeatherPatternsObserver/WeatherPatternsObserver/PressureObserver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherPatternsObserver
+{
+    public class PressureObserver : WeatherStation
+    {
+        public override void updateTemperature(int temperatureData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void updateHumidity(int humidityData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void updatePressure(int pressureData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Display()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
