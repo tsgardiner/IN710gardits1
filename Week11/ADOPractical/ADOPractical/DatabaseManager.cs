08937e48c47c04642dbbfff1800252780b350664
tsgardiner <tsgardiner@windowslive.com>
2016-05-11 09:56:54 +1200

    ADO Initial Commit

---
+++ Week11/ADOPractical/ADOPractical/DatabaseManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADOPractical
+{
+    public class DatabaseManager
+    {
+        SqlConnection bitdevConnection;
+
+
+        public DatabaseManager()
+        {
+            bitdevConnection = new SqlConnection();
+        }
+
+        public void ConnectToDatabase()
+        {
+            bitdevConnection.ConnectionString = "D"
+        }
+
+
+
+    }
+}
