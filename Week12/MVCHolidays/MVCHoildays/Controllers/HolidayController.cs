f2868fc8949d9c23dd536f1fa3691e873f91a3a2
tsgardiner <tsgardiner@windowslive.com>
2016-05-20 07:40:28 +1200

    Holidays MVC Complete

---
+++ Week12/MVCHolidays/MVCHoildays/Controllers/HolidayController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCHoildays.Models;
+
+namespace MVCHoildays.Controllers
+{
+    public class HolidayController : Controller
+    {
+
+        private List<Holiday> holidays;
+        private Holiday selectedHoliday;
+        private Random random;
+
+        // GET: Holiday
+        public ActionResult Index()
+        {   
+            holidays = new List<Holiday>();
+            random = new Random();
+
+            InsertHolidays();
+
+            selectedHoliday = holidays[random.Next(holidays.Count)];
+            
+            CalculateDaysUntil();  
+
+            return View(selectedHoliday);
+        }
+
+        private void InsertHolidays()
+        {
+            CreateHoliday("Queen's Birthday", "queensBirthdayImage.jpg", new DateTime(DateTime.Now.Year, 06, 06));
+            CreateHoliday("Boxing Day", "boxingDayImage.jpg", new DateTime(DateTime.Now.Year, 12, 26));
+            CreateHoliday("Halloween", "halloweenImage.jpg", new DateTime(DateTime.Now.Year, 10, 31));
+        }
+
+        private void CreateHoliday(String name, String image, DateTime date)
+        {
+            holidays.Add(new Holiday(name, image, date));
+        }
+
+        private void CalculateDaysUntil()
+        {
+            selectedHoliday.DaysUntil = (int)(selectedHoliday.Date - DateTime.Now).TotalDays;
+        }
+    }
+}
---
+++ Week12/MVCHolidays/MVCHoildays/Models/Holiday.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCHoildays.Models
+{
+    public class Holiday
+    {
+        public String Name { get; set; }
+        public String Image { get; set; }
+        public DateTime Date { get; set; }
+        public int DaysUntil { get; set; }
+
+
+        public Holiday(String name, String image, DateTime date)
+        {
+            Name = name;
+            Image = image;
+            Date = date;
+        }   
+    }
+
+
+}
