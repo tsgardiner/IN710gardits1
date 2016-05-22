d8d8442026ec2ace965712a48f8b2e126c11ae9b
tsgardiner <tsgardiner@windowslive.com>
2016-05-22 16:48:08 +1200

    Dog Model Created
    Make Dog Database class added
    Enums created

---
+++ Week12/DogSelector/DogSelector/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DogSelector
+{
+    public enum EScale { Low, Medium, High }
+
+    public enum ELength { Short, Medium, Long }
+
+    public enum ESize { Miniature, Small, Medium, Large, Giant }
+}
---
+++ Week12/DogSelector/DogSelector/Models/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DogSelector.Models
+{
+    public class Dog
+    {
+
+        public String BreedName { get; set; }
+        public String DisplayName { get; set; }
+        public EScale ActivityLevel { get; set; }
+        public ELength CoatLength { get; set; }
+        public EScale GroomingLevel { get; set; }
+        public EScale IntelligenceLevel { get; set; }
+        public EScale SheddingLevel { get; set; }
+        public ESize Size { get; set; }
+        public String ImageName { get; set; }
+        public bool Drools { get; set; }
+        public bool GoodWithChildren { get; set; }
+
+        public Dog() {}
+
+    }
+}
