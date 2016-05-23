d750daf2daf886117be9c024180b90460c569285
tsgardiner <tsgardiner@windowslive.com>
2016-05-24 00:07:38 +1200

    Cleared Matcher Class
    Ready for next attempt

--- Week12/DogSelector/DogSelector/DogMatcher.cs
+++ Week12/DogSelector/DogSelector/DogMatcher.cs
@@ -13,30 +13,13 @@ namespace DogSelector
         {
             Dog closestMatch = new Dog();
             List<int> scores = new List<int>();
-            int i = 0;
 
             foreach (Dog dog in allDogs)
             {
-                foreach (var item in dog.GetType().GetProperties())
-                {
-                    var value = item.GetValue(dog, null);
-                    foreach (var wantedItem in desiredDog.GetType().GetProperties())
-                    {
-                        var wantedValue = wantedItem.GetValue(desiredDog, null);
-
-                        if (value == wantedValue)
-                        {
-                            scores[i]++;
-                        }
-                    }
-                        
-                }
-
-                i++;
+                
             }
 
-            int highest = scores.Max();
-            closestMatch = allDogs[highest];
+           
             
             return closestMatch;
         }
