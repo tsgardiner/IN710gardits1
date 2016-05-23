20f78cde2b406a618e6cc9ba74e5038f15687e24
tsgardiner <tsgardiner@windowslive.com>
2016-05-24 00:41:30 +1200

    Dog Matcher Returning Result
    Returns highest scoring dog but still not accurate enough

--- Week12/DogSelector/DogSelector/DogMatcher.cs
+++ Week12/DogSelector/DogSelector/DogMatcher.cs
@@ -13,13 +13,50 @@ namespace DogSelector
         {
             Dog closestMatch = new Dog();
             List<int> scores = new List<int>();
+            int i = 0;
 
             foreach (Dog dog in allDogs)
             {
-                
+                int score = 0;
+                if (desiredDog.ActivityLevel == dog.ActivityLevel)
+                {
+                    scores.Insert(i, score++);
+                }
+                if (desiredDog.CoatLength == dog.CoatLength)
+                {
+                    scores.Insert(i, score++);
+                }
+                if (desiredDog.Drools == dog.Drools)
+                {
+                    scores.Insert(i, score++);
+                }
+                if (desiredDog.GoodWithChildren == dog.GoodWithChildren)
+                {
+                    scores.Insert(i, score++);
+                }
+                if (desiredDog.IntelligenceLevel == dog.IntelligenceLevel)
+                {
+                    scores.Insert(i, score++);
+                }
+                if (desiredDog.SheddingLevel == dog.SheddingLevel)
+                {
+                    scores.Insert(i, score++);
+                }
+                if (desiredDog.Size == dog.Size)
+                {
+                    scores.Insert(i, score++);
+                }
+                if (desiredDog.GroomingLevel == dog.GroomingLevel)
+                {
+                    scores.Insert(i, score++);
+                }
+
+                i++;  
+               
             }
 
-           
+            int maxIndex = scores.IndexOf(scores.Max());
+            closestMatch = allDogs[maxIndex];
             
             return closestMatch;
         }
